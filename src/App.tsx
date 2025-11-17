9c6bf86c01a287c7c59984f1d5c59ca932d63be6
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2025-11-17 15:34:52 +0100

    basic hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import HeroSection from "./components/HeroSection";
 
 function App() {
   return (
     <div className="min-h-screen bg-primary">
       <Navbar />
+
+      <HeroSection />
+
       <Footer />
     </div>
   );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <header className="border flex justify-between items-center p-4">
       <div className="flex items-center justify-between">
-        <h1 className="text-2xl font-bold">Logo</h1>
+        <h1 className="text-2xl font-bold border">Logo</h1>
       </div>
       <MobileNav />
       <nav className="hidden md:flex items-center gap-8">
